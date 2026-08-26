bf36f974523ea4aeae2d38db36f0820699fedf67
LAB12_USER ON WS20 <L12X16W20@LabHCI.net>
2026-08-26 17:52:38 +0800

    Create quiz homescreen

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+ 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+          color: Color.fromARGB(255, 72, 0, 144),
+          ),
+          child: Center(
+            child: Column(mainAxisAlignment:MainAxisAlignment.center,children: [
+              Image.asset('assets/logo.png',
+              width: 180,
+              ),
+              SizedBox(height: 30),
+              Text('Learn Flutter the fun way!',
+              style: TextStyle(
+                fontSize: 26, fontWeight: FontWeight(bold, color: Colors.white,)
+              ))],)
+          ),
+          ),
+        ),
+      ),
+  );
+}
+
